0259e5f932a412808cc46ee7afad4fd073346a2c
Aleph Melo <alephmelo@icloud.com>
2016-04-14 22:50:20 -0300

    Init class.

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+from datetime import datetime
+
+
+class SkyPickerApi(object):
+    """docstring for SkyPickerApi"""
+    def __init__(self):
+        self.base = 'https://api.skypicker.com/'
+        self.path = 'flights?flyFrom=FOR&to=LIS&curr=BRL&'
+        self.date = ''
+        self.time = ''
+
+
