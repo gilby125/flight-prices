5debdbfb9d55d8041ba03c011ae885ebb1e91a4f
Bryan <gilby1@gmail.com>
2016-09-04 14:24:10 -0500

    change interval

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=30)
+@sched.scheduled_job('interval', minutes=5)
 def sched_job():
     call = SkyPickerApi()
     call.save_lowest_price()
